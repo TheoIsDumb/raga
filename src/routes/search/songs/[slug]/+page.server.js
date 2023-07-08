f13759477f12d37221ee6fe39b08a1eb89327dde
theo <lesgooo1993@proton.me>
2023-07-09 00:41:49 +0530

    added /api/search/songs/

--- src/routes/search/songs/[slug]/+page.server.js
+++ src/routes/search/songs/[slug]/+page.server.js
@@ -1,8 +1,8 @@
 import { baseURL, sub } from '$lib/info.js'
 
-export async function load({ params }) {
+export async function load({ params, url }) {
     const slug = params.slug.split('&index=')[0];
-    const index = params.slug.split('&index=')[1] || 1;
+    const index = url.searchParams.get('index') || 1;
     const resp = await fetch(baseURL + sub.index + index + sub.search.songs + slug);
     const data = await resp.json()
     const searchDetails = await data.results;
