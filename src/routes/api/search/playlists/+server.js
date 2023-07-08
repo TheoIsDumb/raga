c0a0068d5e5b394f41ca03fef58d653cc62e4a8b
theo <lesgooo1993@proton.me>
2023-07-09 01:05:03 +0530

    added /api/album?id={id}

--- src/routes/api/search/playlists/+server.js
+++ src/routes/api/search/playlists/+server.js
@@ -1,14 +1,3 @@
-// import { baseURL, sub } from '$lib/info.js'
-
-// export async function load({ params }) {
-//     const resp = await fetch(baseURL + sub.search.playlists + params.slug);
-//     const data = await resp.json()
-//     const playlists = await data.results;
-//     return {
-//         playlists
-//     };
-// }
-
 import { baseURL, sub } from '$lib/info.js'
 import { json } from '@sveltejs/kit';
 
