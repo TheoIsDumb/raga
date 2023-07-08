b7c41112b2ba2a5278502857460c08ab28b61cd9
theo <lesgooo1993@proton.me>
2023-07-09 00:55:05 +0530

    added /api/search/artists

---
+++ src/routes/api/search/artists/+server.js
@@ -0,0 +1,11 @@
+import { baseURL, sub } from '$lib/info.js'
+import { json } from '@sveltejs/kit';
+
+export async function GET({ url }) {
+    const query = url.searchParams.get('q');
+    const resp = await fetch(baseURL + sub.search.artists + query);
+    const data = await resp.json();
+    const results = await data.results;
+
+    return json(results);
+}
