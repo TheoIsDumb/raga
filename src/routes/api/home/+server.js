c6b87918eb14f0951d360291204feb5faabf0d15
theo <lesgooo1993@proton.me>
2023-08-02 14:33:52 +0530

    homedata lang=english

--- src/routes/api/home/+server.js
+++ src/routes/api/home/+server.js
@@ -2,7 +2,12 @@ import { baseURL, sub } from '$lib/info.js'
 import { json } from '@sveltejs/kit';
 
 export async function GET({ url }) {
-    const resp = await fetch(baseURL + sub.modules);
+    const resp = await fetch(baseURL + sub.modules, {
+        method: 'GET',
+        headers: {
+            cookie: 'L=english; gdpr_acceptance=true; DL=english'
+        }
+    });
     const data = await resp.json();
 
     return json(data);
