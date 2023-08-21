20f13171ab650a1cf33a93b92205960bc9637593
theo <lesgooo1993@proton.me>
2023-08-22 02:27:32 +0530

    added adapter-node; build now supports adapter-auto and adapter-node based on DEPLOY_TARGET env.var

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapterAuto from '@sveltejs/adapter-auto'
+import adapterNode from '@sveltejs/adapter-node'
+
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const deployTarget = process.env.DEPLOY_TARGET || 'auto'; // Default to 'auto'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: deployTarget === 'auto' ? adapterAuto() : adapterNode()
 	},
 	preprocess: vitePreprocess()
 };
