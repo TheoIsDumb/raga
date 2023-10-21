06785a06e4ecc1873244e4d66a20af405f39364b
theo <lesgooo1993@proton.me>
2023-10-21 23:03:27 +0530

    load icon

--- src/routes/search/songs/+page.svelte
+++ src/routes/search/songs/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   export let data
   import SongCard from "$lib/components/cards/SongCard.svelte"
+  import Load from '$lib/icons/Load.svelte'
   import { onMount } from 'svelte'
   import { query } from "$lib/store.js"
 
@@ -41,7 +42,7 @@
 
   {#if loading}
     <div class="w-full flex items-center justify-center">
-      <svg xmlns="http://www.w3.org/2000/svg" width="96" height="96" viewBox="0 0 24 24"><circle cx="4" cy="12" r="3" fill="currentColor"><animate id="svgSpinners3DotsFade0" fill="freeze" attributeName="opacity" begin="0;svgSpinners3DotsFade1.end-0.25s" dur="0.75s" values="1;.2"/></circle><circle cx="12" cy="12" r="3" fill="currentColor" opacity=".4"><animate fill="freeze" attributeName="opacity" begin="svgSpinners3DotsFade0.begin+0.15s" dur="0.75s" values="1;.2"/></circle><circle cx="20" cy="12" r="3" fill="currentColor" opacity=".3"><animate id="svgSpinners3DotsFade1" fill="freeze" attributeName="opacity" begin="svgSpinners3DotsFade0.begin+0.3s" dur="0.75s" values="1;.2"/></circle></svg>
+      <Load />
     </div>
   {/if}
 </div>
