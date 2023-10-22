e463d86ca09affc44550730c25eff1c89b7c8b8c
theo <lesgooo1993@proton.me>
2023-10-22 23:32:04 +0530

    header: s/pt-4 pb-2/py-2/ (im dumb); no rounded border input on phone

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -13,13 +13,13 @@
   if ($query === "") $query = $page.url.searchParams.get("q") || "" 
 </script>
 
-<div class="topheader flex w-full pt-4 pb-2">
+<div class="topheader flex w-full py-2">
   <div class="h1_and_input flex w-full justify-between">
     <h1 class="hidden xl:inline text-2xl font-bold">raga.</h1>
 
     <input
-      class="bg-transparent border-2 border-{accentColor} focus:border-[gainsboro]
-        text-center rounded-md xl:rounded w-full xl:w-1/2 xl:h-8 p-1 xl:p-2 outline-none transition duration-300"
+      class="bg-transparent border-[1.5px] xl:border-2 border-{accentColor} focus:border-blue-300
+        text-center xl:rounded w-full xl:w-1/2 xl:h-8 p-1 xl:p-2 outline-none transition duration-300"
       type="text"
       bind:value={$query}
       placeholder="Search..."
