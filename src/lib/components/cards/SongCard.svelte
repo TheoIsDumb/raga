c9eaabdcbdb233d77a70002e997fde080bf6546c
theo <lesgooo1993@proton.me>
2023-10-23 00:55:12 +0530

    lazy hack: added playHigherOrderFunc() to elements, so yeah

--- src/lib/components/cards/SongCard.svelte
+++ src/lib/components/cards/SongCard.svelte
@@ -29,6 +29,10 @@
       $index = i
       $active = $currentPlaylist[$index]
   }
+
+  const playHigherOrderFunc = (item, i) => {
+   type === "song" ? playSong(item) : playSongFromAlbum(item, i)
+  }
 </script>
 
 {#each list as item, i}
@@ -36,12 +40,16 @@
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <!-- svelte-ignore a11y-no-static-element-interactions -->
     <div class="song flex hover items-center w-full overflow-hidden gap-2 rounded"
-    on:click|self={() => type === "song" ? playSong(item) : playSongFromAlbum(item, i)}>
-    <img class="rounded h-14" src={item.image} alt={item.title}/>
+    on:click|self={() => playHigherOrderFunc(item, i)}>
+    <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+    <img class="rounded h-14" src={item.image} alt={item.title}
+    on:click|self={() => playHigherOrderFunc(item, i)} />
 
-    <div class="info flex w-full overflow-hidden justify-between items-center xl:pr-2">
+    <div class="info flex w-full overflow-hidden justify-between items-center xl:pr-2"
+    on:click|self={() => playHigherOrderFunc(item, i)}>
       <div class="flex flex-col leading-3 text-gray-300 text-xs">
-        <div class="font-bold truncate text-sm">{@html item.title}</div>
+        <div class="font-bold truncate text-sm" on:click|self={() => playHigherOrderFunc(item, i)}>
+          {@html item.title}</div>
         <div>
           {#each item.more_info.artistMap.primary_artists as artist, i}
             {@const artists = item.more_info.artistMap.primary_artists}
