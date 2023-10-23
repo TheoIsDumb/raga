9f0119ef258d69b194184bf416c8ccd900ac8615
theo <lesgooo1993@proton.me>
2023-10-23 17:38:32 +0530

    /song/[slug]: simplified layout

--- src/routes/song/[slug]/+page.svelte
+++ src/routes/song/[slug]/+page.svelte
@@ -29,19 +29,19 @@
     }
 </script>
 
-<div class="h-[calc(100dvh-8rem)] w-full flex justify-center items-center flex-col lg:flex-row gap-4">
+<div class="h-[calc(100dvh-8rem)] flex justify-center items-center flex-col gap-2">
   <img class="rounded-lg" src={song.image} alt={song.title}/>
 
-  <div class="info w-full lg:w-[500px] flex flex-col gap-2">
-    <div class="truncate">
+  <div class="info flex flex-col gap-2 items-center">
+    <div class="truncate text-center">
       <div class="font-bold">{@html song.title}</div>
       <div>{@html song.subtitle.split('-')[0]}</div>
-      <div><a class="font-bold" href="/album/{song.more_info.album_id}" alt={song.more_info.album}>{@html song.more_info.album}</a></div>
+      <a class="underline" href="/album/{song.more_info.album_id}" alt={song.more_info.album}>{@html song.more_info.album}</a>
       <div>{@html song.year}</div>
     </div>
 
-    <div class="flex">
-      <button class="cursor-pointer mr-8" on:click={playSong}>
+    <div class="flex gap-8">
+      <button class="cursor-pointer" on:click={playSong}>
         <Play/>
       </button>
 
@@ -49,6 +49,5 @@
         <PlayNext/>
       </button>
     </div>
-
   </div>
 </div>
